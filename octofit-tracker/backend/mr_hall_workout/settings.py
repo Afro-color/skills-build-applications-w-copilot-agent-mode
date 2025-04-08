257c65114129c598910c57109ed217a46bf016f0
Afro-color <152741959+Afro-color@users.noreply.github.com>
2025-04-08 15:59:53 +0000

    Fix manage.py location and static files configuration

--- octofit-tracker/backend/mr_hall_workout/settings.py
+++ octofit-tracker/backend/mr_hall_workout/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -161,3 +162,6 @@ AUTHENTICATION_BACKENDS = [
 # Redirect URLs for login/logout
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
